17f2c2f959e65e5bb4d91263fca6bb41b70d4a59
pavanka <Pavan.Kalmane@ellucian.com>
2023-11-12 10:06:56 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,3 +1,15 @@
-const heading=React.createElement("h1",{id:"heading"},"Hello World React!");
-const root=ReactDOM.createRoot(document.getElementById("root"));    
-root.render(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const heading= React.createElement("h1",{id:"heading"}, "Namaste React");
+const Heading=()=><h1 className ="heading">Namaste React</h1>
+
+const FComponent=()=>{
+    return(
+    <div> <Heading />
+         <h1>this is functional component</h1> 
+    </div>
+    )
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<FComponent />);
---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", {
+    id: heading
+}, "Namaste React");
+
+//# sourceMappingURL=index.6bd02f5a.js.map
